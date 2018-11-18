e0f76906066b5a89c5bcc55c9f937ff667ea7e42
Nilsson82 <Anders858@gmail.com>
2018-11-18 17:54:06 +0100

    Created a player and basic movement for it.

---
+++ Adventure Game/Assets/Scripts/Characters/CharacterBaseControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBaseControl : MonoBehaviour {
+
+  protected CharacterMovementModel m_MovementModel;
+
+  private void Awake()
+  {
+    m_MovementModel = GetComponent<CharacterMovementModel>();
+  }
+
+  protected void SetDirection(Vector2 direction)
+  {
+    m_MovementModel.SetDirection(direction);
+  }
+
+
+}
---
+++ Adventure Game/Assets/Scripts/Characters/CharacterKeyboardControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterKeyboardControl : CharacterBaseControl
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    UpdateDirection();
+
+  }
+
+  void UpdateDirection()
+  {
+    Vector2 newDirection = Vector2.zero;
+
+    if(Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.UpArrow))
+    {
+      newDirection.y = 1;
+    }
+    if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+    {
+      newDirection.y = -1;
+    }
+    if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+    {
+      newDirection.x = -1;
+    }
+    if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+    {
+      newDirection.x = 1;
+    }
+
+    SetDirection(newDirection);
+
+
+  }
+}
